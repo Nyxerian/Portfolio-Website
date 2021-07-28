2fa72c5e2d63cad13a1a127b4f96a8ea3bc6eac3
Nyxerian <DanielleNyxDaSilva@gmail.com>
2021-07-28 12:48:11 +0200

    Added Project - DnD website

--- src/Pages/4. Projects Page/Projects.js
+++ src/Pages/4. Projects Page/Projects.js
@@ -1,7 +1,7 @@
 import NavBar from "../../Components/Navbar/NavBar";
 import "./Projects.css";
 import { FaReact } from 'react-icons/fa'
-import { SiHtml5, SiCss3, SiJavascript } from 'react-icons/si'
+import { SiHtml5, SiCss3, SiJavascript, SiJquery } from 'react-icons/si'
 
 export default function Projects() {
   return (
@@ -38,12 +38,44 @@ export default function Projects() {
                   </div>
                 </div>
               </div>
-            </div>
+            </div> 
 
-            
+            <div class="card-container">
+              <div class="card-top DnD">
+                <span class="project-type">D&D Website</span>
+                <div class="blur"></div>
+              </div>
+              <div class="progress DnD">
+                <span> 100 %</span>
+              </div>
+              <div class="content">
+              <br /><br />
+                <div class="summary">
+                  Bootcamp Capstone Project - create a website about a passion of yours.
+                </div>
+                <a href="https://nyxerian.github.io/Capstone-Project-3-Mod1-Task15/" class="btn"> Open Project </a>
+                <div class="project-info">
+                  <div class="info">
+                    <span>{<SiJquery />}</span>
+                    <span>JQuery</span>
+                  </div>
+                  <div class="info">
+                    <span>{<SiHtml5 />}{<SiCss3 />}</span>
+                    <span>HTML/CSS</span>
+                  </div>
+                  <div class="info">
+                    <span>{<SiJavascript />}</span>
+                    <span>JavaScript</span>
+                  </div>
+                </div>
+              </div>
+            </div>  
 
-            
           </div>
+
+
+
+
     </div>
         
   );
